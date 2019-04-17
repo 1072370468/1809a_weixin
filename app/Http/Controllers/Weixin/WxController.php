51cc8dbb9d5bb129d5eaee0b2bdcf3fedcf39dbd
1072370468 <1072370468@qq.com>
2019-04-17 08:03:09 +0000

    weixin

---
+++ app/Http/Controllers/Weixin/WxController.php
@@ -0,0 +1,143 @@
+<?php
+namespace App\Http\Controllers\Weixin;
+
+use GuzzleHttp\Psr7\Uri;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Support\Facades\Redis;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
+
+use App\Model\WeixinUserModel;
+
+class WxController extends Controller
+{
+    //首次接入
+    public function valid(){
+        echo $_GET['echostr'];
+    }
+ 
+    /* 
+    接收微信推送事件
+    */
+  /*   public function wxEvent()
+    {
+        //使用Guzzle
+        $client = new Client();
+        $xml_str = file_get_contents("php://input");
+        $log_str = '>>>>>>>>>'.date("Y-m-d H:i:s").$xml_str."\n";
+        file_put_contents("logs/wx_event.log",$log_str,FILE_APPEND);
+        //日志文件
+        $xml_obj = simplexml_load_string($xml_str);
+        //处理业务逻辑
+        $msg_type=$xml_obj->MsgType;    //消息类型
+        $open_id = $xml_obj->FromUserName;  //用户openid
+        $app = $xml_obj->ToUserName;    //公众号ID
+        if($msg_type=='image'){
+            $media_id = $xml_obj->MediaId;
+            
+            //MediaId URL
+            $url ='https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->getAccessToken().'&media_id='.$media_id;
+            $response=$client->get(new Uri($url));
+
+            $headers = $response->getHeaders();     //获取 响应 头信息
+            $file_info =$headers['Content-disposition'][0];     //获取文件名
+
+            $file_name = rtrim(substr($file_info,-20),'"');
+            $new_file_name='weixin/'.substr(md5(time().mt_rand()),10,8).'_'.$file_name;
+            //保存文件
+            $rs=Storage::put($new_file_name,$response->getBody());
+            //保存文件
+            if($rs){
+                //TODO 保存成功
+            }else{
+                //TODO 保存失败
+            }
+        }elseif($msg_type=='voice'){
+            $media_id = $xml_obj->MediaId;
+            $url = 'https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$this->getAccessToken().'&media_id='.$media_id;
+            $amr =file_get_contents($url);
+            $file_name=time().mt_rand(11111,99999).'.amr';
+            $rs = file_put_contents('wx/voice.'.$file_name,$amr);//保存录音文件
+            var_dump($rs);
+        }elseif($msg_type=='text'){
+            //自动回复天气
+            if(strpos($xml_obj->Content,'+天气')){
+                $city = explode('+',$xml_obj->Content)[0];
+                $url = 'https://free-api.heweather.net/s6/weather/now?key=HE1904160951011886&location='.$city;
+                $arr=json_decode(file_get_contents($url),true);
+                if($arr['HeWeather6'][0]['status']=='ok'){//城市名是否正确
+                    $fl=$arr['HeWeather6'][0]['now']['tmp'];//摄氏度
+                    $wind_dir=$arr['HeWeather6'][0]['now']['wind_dir'];//风向
+                    $wind_dir=$arr['HeWeather6'][0]['now']['wind_sc']//风力
+                    $wind_dir=$arr['HeWeather6'][0]['now']['hum'];//湿度
+                    $str = "城市：$city \n" ."温度: ".$fl."\n" . "风向：". $wind_dir ."\n" . "风力：".$wind_sc . "\n湿度：".$hum."\n";
+                    $response_xml = '<xml>
+                    <ToUserName><![CDATA['.$open_id.']]></ToUserName>
+                    <FromUserName><![CDATA['.$app.']]></FromUserName>
+                    <CreateTime>'.time().'</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA['.$str.']]></Content>
+                  </xml>';
+                }else{
+                    $response_xml = '<xml>
+                    <ToUserName><![CDATA['.$open_id.']]></ToUserName>
+                    <FromUserName><![CDATA['.$app.']]></FromUserName>
+                    <CreateTime>'.time().'</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[城市名不正确]]></Content>
+                  </xml>';
+                }
+                echo $response_xml;
+            }
+        }
+    }
+    public function getAccessToken(){
+        //先获取缓存如果不存在则请求接口
+        $redis_key='wx_access_token';
+        $token=Redis::get($redis_key);
+        if($token){
+            echo 'Cache:';echo '</br>';
+        }else{
+            echo 'No Cache';echo '</br>';
+            $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env("WX_APPID").'&secret='.env("WX_APP_SECRET");
+            $json_str = file_get_contents($url);
+            $arr = json_decode($json_str,true);
+            echo '<pre>';print_r($arr);echo'</pre>';
+            
+            Redis::set($redis_key,$arr['access_token']);
+            Redis::expire($redis_key,3600) //设置过期时间
+            $token = $arr['access_token'];
+        }
+        return $token;
+    }
+    //根据openid消息群发
+    public function sendMsg($openid_arr,$content){
+        $msg=[
+            "touser"=>$openid_arr,
+            "msgtype"=>"texf",
+            "text"=>[
+                "content" => $content
+            ]
+        ];
+        $data=json_encode($msg,JSON_UNESCAPED_UNICODE);
+        //openid 群发接口
+        $url='https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token='.$this->getAccessToken();
+        $client=new Client();
+        $response=$client->request('post',$url,[
+            'body'=>$data
+        ]);
+        return $response->getBody();
+    }
+    public function send(){
+    $user_list=WeixinUserModel::where(['sub_status'=>1])->get()->toArray();
+    $openid_arr = array_column($user_list,'openid');
+    echo '<pre>';print_r($openid_arr);echo'<pre>';
+    $msg="今天是周二";
+    $response=$this->sendMsg($open_arr,$msg);
+    echo $response;
+    } */
+
+}
+?>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/weixin/valid','Weixin\WxController@valid');       //首次接入
+Route::post('/weixin/valid','Weixin\WxController@wxEvent');       //接收推送事件
+// Route::get('/weixin/token','Weixin\WxController@getAccessToken');       //获取access_token
+
+// Route::get('/weixin/send','Weixin\WxController@send');       //消息群发
+
+
